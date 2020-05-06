80601f413bab73be0f46f2c4514a551657cf3d57
Hugo Villain <hugo.villain@prismic.io>
2020-05-06 18:54:40 +0200

    add hideLayout prop

--- modules/Layout.js
+++ modules/Layout.js
@@ -63,6 +63,7 @@ export const WithLayout = ({
           <Provider
             {...{
               ContainerProps: defaultContainerProps,
+              hideContainer,
               ...Comp,
             }}
           >
