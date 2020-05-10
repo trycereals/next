c7897b929c9787223723ab87e592ec8f6e5f222c
Hugo Villain <hugo.villain@prismic.io>
2020-05-10 20:56:15 +0200

    update commit script

--- scripts/prepare-commit.js
+++ scripts/prepare-commit.js
@@ -8,10 +8,10 @@ if (sh.test('-d', 'app/theme')) {
   sh.cp("app/theme.json", path.join(process.cwd(), "../"));
 }
 
-if (sh.test('-d', './public')) {
-  sh.cp('-R', './public', path.join(process.cwd(), '..'))
-  sh.rm("-R", path.join(process.cwd(), "public/*"));
-}
+// if (sh.test('-d', './public')) {
+//   sh.cp('-R', './public', path.join(process.cwd(), '..'))
+//   sh.rm("-R", path.join(process.cwd(), "public/*"));
+// }
 
 if (sh.test('-d', 'app/customs')) {
   sh.cp('-R', 'app/customs', path.join(process.cwd(), '../'))
@@ -22,4 +22,5 @@ if (sh.test('-d', 'app/customs')) {
 if (sh.test('-e', 'app/layout')) {
   sh.cp('-R', 'app/layout', path.join(process.cwd(), '../'))
   sh.rm('../layout/index.js')
+  sh.rm('../layout/components/index.js')
 }
